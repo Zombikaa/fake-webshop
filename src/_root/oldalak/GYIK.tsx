69c81687f9c954dcdb2cca30a7f53b2bd084ae0d
Zombikaa <153750844+Zombikaa@users.noreply.github.com>
2024-05-29 09:12:41 +0200

    Create GYIK.tsx

---
+++ src/_root/oldalak/GYIK.tsx
@@ -0,0 +1,23 @@
+const GYIK = () => {
+  return (
+    <div>
+      <h1 className="text-center font-extrabold text-[40px]">Gyakran Ismételt Kérdések</h1>
+        <br>
+        <div>
+            <h3 className="font-bold text-xl">Lorem?</h3>
+            <p className="italic">Lorem ipsum dolor sit amet :)</p>
+            <br>
+              <h3 className="font-bold text-xl">Lorem?</h3>
+            <p className="italic">Lorem ipsum dolor sit amet :)</p>
+              <br>
+              <h3 className="font-bold text-xl">Lorem?</h3>
+            <p className="italic">Lorem ipsum dolor sit amet :)</p>
+                <br>
+              <h3 className="font-bold text-xl">Lorem?</h3>
+            <p className="italic">Lorem ipsum dolor sit amet :)</p>
+        </div>
+    </div>
+  );
+};
+
+export default GYIK;
